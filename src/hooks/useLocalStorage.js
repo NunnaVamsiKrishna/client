2d076298c1e13069badc140f18417e5acffc8372
Vamsi Krishna <2000030732@kluniversity.in>
2022-11-27 00:30:14 +0530

    Intial Commit

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,22 @@
+import {useState,useEffect} from 'react'
+
+const PREFIX='whatsapp-clone-'
+
+export default function LocalStorage(key,intitalValue){
+  const prefixedKey=PREFIX+key
+  const [value,setValue]=useState(()=>{
+    const jsonValue=localStorage.getItem(prefixedKey)
+    if(jsonValue!=null) return JSON.parse(jsonValue)
+    if(typeof intitalValue==='function'){
+      return intitalValue()
+    }else{
+      return intitalValue
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(prefixedKey,JSON.stringify(value))
+    }, [prefixedKey, value])
+  
+    return [value,setValue]
+}
